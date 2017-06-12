14fd1b9f62393bcb384e61aa3ee8342552be3fcd
Parankush <parankush@me.com>
2017-06-12 14:13:57 -0400

    Initial Commit

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Parankush Bhardwaj on 6/12/17.
+//  Copyright © 2017 Parankush Bhardwaj. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
